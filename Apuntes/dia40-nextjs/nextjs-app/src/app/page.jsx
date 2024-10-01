545d67503e962ef5b3135a2671c5a9f26de03427
aidin-rodriguez <rodriguezaidin@alumnos.cei.es>
2024-10-01 12:07:59 +0200

    nuevo tema

---
+++ Apuntes/dia40-nextjs/nextjs-app/src/app/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { useState } from "react";
+
+export default function Home() {
+  return (
+   <h1>Hola spy el home</h1>
+  );
+}
---
+++ Apuntes/dia40-nextjs/nextjs-app/src/components/Header.jsx
@@ -0,0 +1,7 @@
+import Link from 'next/link'
+
+const Header = () => {
+    return (
+        <li><Link href="/task/5">Tarea 5</Link></li>
+    )
+}
