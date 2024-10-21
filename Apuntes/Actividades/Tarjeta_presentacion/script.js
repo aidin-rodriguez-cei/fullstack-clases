692b91764b0f8899fa8989f7035f27e49186970f
AidinR <rodriguezaidin@alumnos.cei.es>
2024-10-21 07:14:31 +0200

    cambios

--- Apuntes/Actividades/Tarjeta_presentacion/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('curiosityBtn').addEventListener('click', function() {
-    let curiosity = document.getElementById('curiosity');
-    if (curiosity.innerText === "") {
-        curiosity.innerText = "Le gustaría emprender algún negocio web en el futuro 🚀";
-    } else {
-        curiosity.innerText = "";
-    }
-});
